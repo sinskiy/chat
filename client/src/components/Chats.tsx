e21e585cd092d75b427d896be38165eab63e8305
sinskiy <sinskiy@proton.me>
2024-09-06 19:46:40 +0300

    feat(client): show empty chats message

--- client/src/components/Chats.tsx
+++ client/src/components/Chats.tsx
@@ -4,11 +4,15 @@ import { User } from "../context/UserContext";
 const Chats = ({ users }: { users: User[] }) => {
   return (
     <nav>
-      {users.map((user) => (
-        <Link key={user.id} to={`/?userId=${user.id}`}>
-          {user.username}
-        </Link>
-      ))}
+      {users.length > 0 ? (
+        users.map((user) => (
+          <Link key={user.id} to={`/?userId=${user.id}`}>
+            {user.username}
+          </Link>
+        ))
+      ) : (
+        <p>no chats yet</p>
+      )}
     </nav>
   );
 };
