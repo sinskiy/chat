08d1c51b0326e0c1c130533553245950505d8792
sinskiy <sinskiy@proton.me>
2024-09-08 14:59:05 +0300

    feat(client): create groups

--- client/src/routes/NewGroup.tsx
+++ client/src/routes/NewGroup.tsx
@@ -1,11 +1,45 @@
+import { FormEvent, useContext, useEffect } from "react";
 import Form from "../components/Form";
 import InputField from "../components/InputField";
+import useFetch from "../hooks/useFetch";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const NewGroup = () => {
+  const navigate = useNavigate();
+
+  const { data, fetchData, error, isLoading } = useFetch();
+
+  const { user } = useContext(UserContext);
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    fetchData("/groups", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({
+        name: data.get("name"),
+        userId: user?.id,
+      }),
+      credentials: "include",
+    });
+  }
+
+  useEffect(() => {
+    if (data) {
+      navigate(`/groups/${data.group.id}/request`);
+    }
+  }, [data]);
+
   return (
     <section className="centered-section">
       <h1>New group</h1>
-      <Form isLoading={false}>
+      <Form isLoading={isLoading} onSubmit={handleSubmit}>
+        {error && <p aria-live="polite">{error}</p>}
+        {isLoading && <p>loading...</p>}
         <InputField label="name" required minLength={1} maxLength={30} />
       </Form>
     </section>
