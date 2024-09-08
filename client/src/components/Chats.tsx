1b0115e4a91a46e35c93eb2ef5f364085d60dac7
sinskiy <sinskiy@proton.me>
2024-09-08 14:33:26 +0300

    feat(client): add static new group button

--- client/src/components/Chats.tsx
+++ client/src/components/Chats.tsx
@@ -1,6 +1,7 @@
 import { SetURLSearchParams } from "react-router-dom";
 import { User } from "../context/UserContext";
 import classes from "./Chats.module.css";
+import { Plus } from "lucide-react";
 
 interface ChatsProps {
   users: User[];
@@ -10,26 +11,32 @@ interface ChatsProps {
 
 const Chats = ({ users, searchParams, setSearchParams }: ChatsProps) => {
   return (
-    <nav className={classes.chats}>
-      {users.length > 0 ? (
-        users.map((user) => (
-          <button
-            key={user.id}
-            onClick={() => setSearchParams({ "partner-id": String(user.id) })}
-            className={[
-              classes.chat,
-              Number(searchParams.get("partner-id")) === user.id
-                ? "primary"
-                : "surface",
-            ].join(" ")}
-          >
-            {user.username}
-          </button>
-        ))
-      ) : (
-        <p>no chats yet</p>
-      )}
-    </nav>
+    <>
+      <nav className={classes.chats}>
+        <button className={[classes.chat, classes.group, "primary"].join(" ")}>
+          <Plus size={20} />
+          new group
+        </button>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <button
+              key={user.id}
+              onClick={() => setSearchParams({ "partner-id": String(user.id) })}
+              className={[
+                classes.chat,
+                Number(searchParams.get("partner-id")) === user.id
+                  ? "primary"
+                  : "surface",
+              ].join(" ")}
+            >
+              {user.username}
+            </button>
+          ))
+        ) : (
+          <p>no chats yet</p>
+        )}
+      </nav>
+    </>
   );
 };
 
