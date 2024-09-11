d2d3a8359d0cac61699654ea5f69cd9d4580814b
sinskiy <sinskiy@proton.me>
2024-09-11 18:46:02 +0300

    Revert "fix(server): make message recipient non-optional"
    This reverts commit 6c17c5cb607ea02c452ecffefecb3360848590b1.

--- server/prisma/migrations/20240908133457_recipient_non_optional/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `recipientId` on table `Message` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Message" ALTER COLUMN "recipientId" SET NOT NULL;
