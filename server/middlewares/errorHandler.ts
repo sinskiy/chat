d5a1aa4b02a5eaa9377e467780776941e7942cff
sinskiy <sinskiy@proton.me>
2024-09-12 18:37:33 +0300

    fix(server): always send number as an error status

--- server/middlewares/errorHandler.ts
+++ server/middlewares/errorHandler.ts
@@ -8,7 +8,7 @@ export default function errorHandler(
 ) {
   console.error(err.message, err.stack);
 
-  const statusCode = err.statusCode ?? 500;
+  const statusCode = Number(err.statusCode) || 500;
   res.status(statusCode).json({ error: err.message });
 }
 
